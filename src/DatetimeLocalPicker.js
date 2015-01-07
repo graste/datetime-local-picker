2c3fa506887782f80cf66320565f2a1e7a9d13de
Steffen Gransow <graste@mivesto.de>
2015-01-07 19:07:24 +0100

    add minDate/maxDate settings support

--- src/DatetimeLocalPicker.js
+++ src/DatetimeLocalPicker.js
@@ -23,12 +23,22 @@
         triggerElement: null,
         containerElement: null,
         utcOffsetElement: null,
-        locale: null,
 
-        inputFormats: null,
-        outputFormat: null,
+        locale: 'en',
+
+        //inputFormats: null,
+        inputFormats: [
+            moment.ISO_8601,
+            "YYYY-MM-DD[T]HH:mm:ss.SSSZ",
+            "YYYY-MM-DD[T]HH:mm:ssZ",
+            "YYYY-MM-DD",
+            "HH:mm:ss.SSS",
+            "HH:mm:ss"
+        ],
+        displayFormat: moment.ISO_8601,
+        outputFormat: moment.ISO_8601,
         parseStrict: false,
-        parseHuman: false,
+        //parseHuman: false,
 
         defaultDate: null,
 
@@ -45,8 +55,8 @@
         minYear: 0,
         maxYear: 3000,
         yearRange: [0, 10],
-        minMonth: undefined,
-        maxMonth: undefined,
+        minMonth: 0,
+        maxMonth: 11,
 
         isRTL: false,
 
@@ -75,8 +85,6 @@
 
         disabledDays: [ new Date(), new Date() ],
         disableDayFunctions: [],
-
-        validDayFunctions: [],
 
         disableWeekends: false,
 
@@ -161,12 +169,10 @@
             nextMonth: '',
 
             months: [],
-            monthsMedium: [],
             monthsShort: [],
 
             weekdays: [],
             weekdaysShort: [],
-            weekdaysMedium: [],
 
             midnight: '',
             noon: '',
@@ -190,6 +196,48 @@
 
     DatetimeLocalPicker.prototype = {
 
+        parseDate: function(mixed) {
+            var new_date = moment(mixed).locale(this.settings.locale);
+            return new_date.isValid() ? new_date : false;
+        },
+
+        setMinDate: function(mixed) {
+            this.settings.minDate = this.parseDate(this.settings.minDate);
+            return this.consolidateMinMaxDates();
+        },
+
+        setMaxDate: function(mixed) {
+            this.settings.maxDate = this.parseDate(this.settings.maxDate);
+            return this.consolidateMinMaxDates();
+        },
+
+        consolidateMinMaxDates: function() {
+            // swap min/max dates if they are in "wrong" order
+            if ((this.settings.minDate && this.settings.maxDate) && this.settings.maxDate.isBefore(this.settings.minDate)) {
+                var temp = this.settings.maxDate.clone();
+                this.settings.maxDate = this.settings.minDate.clone();
+                this.settings.minDate = temp;
+            }
+
+            if (this.settings.minDate) {
+                this.settings.minYear = this.settings.minDate.year();
+                this.settings.minMonth = this.settings.minDate.month();
+            } else {
+                this.settings.minYear = default_settings.minYear;
+                this.settings.minMonth = default_settings.minMonth;
+            }
+
+            if (this.settings.maxDate) {
+                this.settings.maxYear = this.settings.maxDate.year();
+                this.settings.maxMonth = this.settings.maxDate.month();
+            } else {
+                this.settings.maxYear = default_settings.maxYear;
+                this.settings.maxMonth = default_settings.maxMonth;
+            }
+
+            return this;
+        },
+
         configure: function(settings) {
             if (!this.settings) {
                 this.settings = $.extend(true, {}, defaults);
@@ -198,14 +246,30 @@
             this.settings = $.extend(true, {}, default_settings, this.settings, settings);
 
             // add (randomized) log prefix to instance
-            this.log_prefix = this.settings.log_prefix || 'DatetimeLocalPicker';
-            if (!this.settings.randomize_log_prefix || this.settings.randomize_log_prefix === true || this.settings.log_prefix === "") {
-                this.log_prefix += "#" + getRandomString();
+            this.settings.logPrefix = this.settings.logPrefix || 'DatetimeLocalPicker';
+            if (!this.settings.randomizeLogPrefix || this.settings.randomizeLogPrefix === true || this.settings.logPrefix === '') {
+                this.settings.logPrefix += '#' + getRandomString();
             }
 
             this.settings.isRTL = !!this.settings.isRTL;
 
+            // check for a valid given locale to use and store that in settings (as it might be invalid/unknown)
+            this.settings.locale = moment().locale(this.settings.locale);
 
+            // use the input element as trigger element when no specific trigger was given
+            this.settings.triggerElement = this.triggerElement || this.settings.inputElement;
+
+            //if (!_.isArray(this.settings.inputFormats)) {
+            //    throw new Error('The inputFormats must be an array of dates or date strings.');
+            //}
+
+            this.settings.disableWeekends = !!this.settings.disableWeekends;
+
+            var nom = parseInt(this.settings.numberOfMonths, 10) || 1;
+            this.settings.numberOfMonths = nom > 12 ? 12 : nom;
+
+            this.setMinDate(this.settings.minDate);
+            this.setMaxDate(this.settings.maxDate);
         }
 
     };
