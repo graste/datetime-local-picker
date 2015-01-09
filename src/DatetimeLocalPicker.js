490d995591ccb510f57562e14e4680bd8157f9d8
Steffen Gransow <graste@mivesto.de>
2015-01-09 16:42:15 +0100

    add weekdays rendering (calendar header) and add
    is-invalid css class when a date cannot be parsed with
    the given inputFormats on keyUp event. Output element
    value should stay correct almost all times. Invalid
    values onChange will lead to the last known valid date
    being written back to the input element.

--- src/DatetimeLocalPicker.js
+++ src/DatetimeLocalPicker.js
@@ -17,8 +17,6 @@
 }(function ($, _, moment) {
     'use strict';
 
-    var $output = $('#output');
-
     var default_settings = {
         inputElement: null,
         triggerElement: null,
@@ -26,6 +24,12 @@
         utcOffsetElement: null,
 
         locale: 'en',
+        parseStrict: true,
+        //parseHuman: false,
+        firstDayOfWeek: null,
+        weekendDays: [5, 6],
+        notationWeekdays: '_weekdaysMin',
+        numberOfMonths: 1,
 
         inputFormats: [
             moment.ISO_8601,
@@ -45,21 +49,19 @@
             "DD/MM/YYYY HH:mm",
             "DD/MM/YYYY"
         ],
-        // displayFormat: 'YYYY-MM-DD HH:mm:ss',
+
+        //displayFormat: 'YYYY-MM-DD HH:mm:ss',
         displayFormat: 'L LTS',
+
         outputFormat: 'YYYY-MM-DD[T]HH:mm:ss.SSSZ',
-        parseStrict: false,
-        //parseHuman: false,
-
-        defaultDate: null,
-
-        defaultHours: 0,
-        defaultMinutes: 0,
-        defaultSeconds: 0,
-        defaultMilliseconds: 0,
-
-        firstDayOfWeek: 0,
-        numberOfMonths: 1,
+        output: {
+            element: null,
+            format: 'YYYY-MM-DD[T]HH:mm:ss.SSSZ',
+            forceHours: null,
+            forceMinutes: null,
+            forceSeconds: null,
+            forceMilliseconds: null
+        },
 
         constraints: {
             minDate: null,
@@ -69,6 +71,16 @@
             minMonth: 0,
             maxMonth: 11
         },
+
+
+
+
+
+        defaultDate: null,
+        defaultHours: 0,
+        defaultMinutes: 0,
+        defaultSeconds: 0,
+        defaultMilliseconds: 0,
 
         yearRange: [0, 10],
 
@@ -84,7 +96,7 @@
         showSeconds: false,
         showMilliseconds: false,
 
-        highlightDays: [
+        highlight: [
             {
                 date: '2015-01-24',
                 css: 'holidays',
@@ -95,19 +107,18 @@
                 css: 'event'
             }
         ],
-        highlightDayFunctions: [],
+        //highlightFunctions: [],
 
-        disabledDays: [ new Date(), new Date() ],
-        disableDayFunctions: [],
-
+        disable: [ new Date(), new Date() ],
+        //disableFunctions: [],
         disableWeekends: false,
+
+        disableInput: false,
 
         position: 'bottom left',
         autoReposition: true,
         shouldReflow: true,
 
-        disableInput: false,
-        clearOnInvalidInput: false,
 
         use24hour: true,
         hourFormat: '',
@@ -194,8 +205,6 @@
 
             clear: '',
             today: '',
-
-            someMore: ''
         },
 
         even: 'more'
@@ -214,10 +223,9 @@
 
     function getDaysInMonth(moment_or_year, month) {
         if (moment.isMoment(moment_or_year)) {
-            return moment_or_year.endOf('month').date(); // last day of given moment's month
+            return moment_or_year.daysInMonth();
         }
-        // create ISO date from given year and month and determine last day of that date's month
-        return moment(''+(+moment_or_year)+'-'+(+month)+'-01', 'YYYY-MM-DD').endOf('month').date();
+        return moment(''+(+moment_or_year)+'-'+(+month), 'YYYY-MM').daysInMonth();
     }
 
     // constructor function for DatetimeLocalPicker instances
@@ -237,13 +245,17 @@
 
         $input_element.removeAttr('name');
 
-        setCurrentDate(parseDate($input_element.val()));
-        console.log('utc', moment(current_date).utc().format());
-        console.log('local', moment(current_date).local().format());
-        current_date.local();
-        $input_element.val(moment(current_date).format(settings.displayFormat));
-
         bindEventHandlers();
+
+        var initial_value = $input_element.val();
+        var initial_date = parseDate(initial_value);
+        if (moment.isMoment(initial_date) && initial_date.isValid()) {
+            current_date = moment(initial_date);
+            setCurrentDate(initial_date);
+        } else {
+            current_date = moment();
+            current_date.locale(settings.locale);
+        }
 
         // return public api
         return {
@@ -269,21 +281,16 @@
             },
             getSettings: function() {
                 return _.cloneDeep(settings, cloneSpecialValues);
+            },
+            getCurrentDate: function() {
+                return moment(current_date);
             }
         };
 
         function parseDate(mixed) {
-            return moment(mixed, settings.inputFormats).locale(settings.locale);
-        }
-
-        function draw() {
-            if ($container_element) {
-                $container_element.html(
-                    settings.templates.calendar({
-                        date: moment(current_date).format('MMMM YYYY')
-                    })
-                );
-            }
+            var d = moment(mixed, settings.inputFormats, settings.locale, settings.parseStrict);
+            d.locale(settings.locale);
+            return d;
         }
 
         function bindEventHandlers() {
@@ -305,10 +312,25 @@
             });
 
             $hidden_element.on('change.' + settings.logPrefix, function(ev) {
-                console.log($hidden_element.val());
+                console.log('hidden value: ' + $hidden_element.val());
             });
+
             $input_element.on('change.' + settings.logPrefix, handleInputElementChange);
-            //$input_element.on('keyup.' + settings.logPrefix, handleInputElementChange);
+
+            $input_element.on(
+                'pointerup.' + settings.logPrefix + ' ' +
+                'keyup.' + settings.logPrefix + ' ',
+                handleInputElementPointerUp
+            );
+        }
+
+        function handleInputElementPointerUp(ev) {
+            var date = parseDate($input_element.val());
+            if (moment.isMoment(date) && date.isValid()) {
+                $input_element.removeClass('is-invalid');
+            } else {
+                $input_element.addClass('is-invalid');
+            }
         }
 
         function handleInputElementChange(ev) {
@@ -317,10 +339,75 @@
 
         function setCurrentDate(date) {
             if (moment.isMoment(date) && date.isValid()) {
-                current_date = moment(date);
-                $hidden_element.val(moment(current_date).utc().format(settings.outputFormat));
-                draw();
+                setHiddenElementDate(date);
+                setInputElementDate(date);
+            } else {
+                resetInputElementDate();
             }
+
+            draw();
+        }
+
+        function setHiddenElementDate(valid_moment) {
+            $hidden_element.val(
+                //moment(valid_moment).utc().format(settings.outputFormat)
+                moment(valid_moment).toISOString()
+            );
+        }
+
+        function setInputElementDate(valid_moment) {
+            $input_element.val(
+                moment(valid_moment).local().format(settings.displayFormat)
+            );
+        }
+
+        // set input element value to the last known valid date
+        function resetInputElementDate() {
+            var m = parseDate($hidden_element.val());
+            if (moment.isMoment(m) && m.isValid()) {
+                $input_element.val(
+                    m.local().format(settings.displayFormat)
+                );
+            } else {
+                throw new Error('Hidden input element contains an invalid moment: ' + m);
+            }
+        }
+
+        function draw() {
+            if ($container_element) {
+                $container_element.html(
+                    settings.templates.calendar({
+                        date: moment(current_date).format('MMMM YYYY'),
+                        weekdays: prepareWeekdays()
+                    })
+                );
+            }
+        }
+
+        function prepareWeekdays() {
+            var weekdays = [];
+            var m = moment(current_date);
+            var locale_data = m.localeData();
+            var idx = 0;
+            for (idx = 0; idx < 7; idx++) {
+                // determine actual index (day number) to lookup in weekdaysShort
+                var day = idx + settings.firstDayOfWeek;
+                while (day >= 7) {
+                     day -= 7;
+                }
+
+                var css_classes = 'weekday';
+                if (settings.weekendDays.indexOf(idx) !== -1) {
+                    css_classes += ' weekend ';
+                }
+
+                weekdays[idx] = {
+                    content: locale_data[settings.notationWeekdays][day],
+                    css: css_classes
+                };
+            }
+
+            return weekdays;
         }
 
         function removeEventHandlers() {
@@ -341,8 +428,28 @@
 
             // check for a valid given locale to use and store that in settings (as it might be invalid/unknown)
             var m = moment();
-            var locale = m.locale(settings.locale);
-            settings.locale = m.locale();
+            m.locale(settings.locale);
+            settings.locale = m.locale(); // actual locale being used
+
+            var fdow = settings.firstDayOfWeek;
+            if (!_.isNull(fdow)) {
+            }
+            if (!_.isNull(fdow) && ((+fdow >= 0) && (+fdow <= 6))) {
+                settings.firstDayOfWeek = fdow;
+            } else {
+                var l = m.localeData();
+                settings.firstDayOfWeek = l.firstDayOfWeek();
+            }
+
+            if (!(_.isArray(settings.weekendDays) && _.min(settings.weekendDays) >= 0 && _.max(settings.weekendDays) <= 6)) {
+                throw new Error('The weekendDays must be an array of integers that represent the index of weekdays to use as weekend (defaults to [5, 6]).');
+            }
+
+            var valid_weekdays_notations = ['_weekdays', '_weekdaysShort', '_weekdaysMin'];
+            if (!_.isString(settings.notationWeekdays) || (_.isString(settings.notationWeekdays) && valid_weekdays_notations.indexOf(settings.notationWeekdays) === -1)) {
+                throw new Error('The notationWeekdays must be one of: ' + valid_weekdays_notations.join(', '));
+            }
+            settings.notationWeekdays = settings.notationWeekdays || '_weekdaysMin';
 
             if (!_.isArray(settings.inputFormats)) {
                 throw new Error('The inputFormats must be an array of acceptable date formats for moment.');
